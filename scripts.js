7757a8f5e7913168d3c5b5aba05aa5a507775f17
JullioDutra <134459685+JullioDutra@users.noreply.github.com>
2024-04-12 21:21:33 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("palmeiras").addEventListener("click", function() {
+    // Redirecionar para a página desejada
+    window.location.href = "Times/Palmeiras.html";
+});
+
+document.getElementById("vasco").addEventListener("click", function() {
+    // Redirecionar para a página desejada
+    window.location.href = "Times/Vasco.html";
+});
+
+document.getElementById("botafogo").addEventListener("click", function() {
+    // Redirecionar para a página desejada
+    window.location.href = "Times/Botafogo.html";
+});
